d56ad10b2b3bf3bf2f82cb3d0ecb652bdf7db9e6
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2025-09-25 16:20:37 +0100

    dunno

--- apps/os/vitest.eval.config.ts
+++ apps/os/vitest.eval.config.ts
@@ -1,17 +1,52 @@
 import * as fs from "fs";
 import { defineConfig } from "vitest/config";
 import EvaliteReporter from "evalite/reporter";
+import { type Evalite } from "evalite/types";
 import betterSqlite3 from "better-sqlite3";
-import type { ad } from "vitest/dist/chunks/reporters.d.BFLkQcL6.js";
+import type { RunnerTask, RunnerTestCase } from "vitest";
 
 fs.mkdirSync("ignoreme", { recursive: true });
 const db = betterSqlite3("ignoreme/eval.db");
 
+type MethodInputs = {
+  [K in keyof EvaliteReporter]: EvaliteReporter[K] extends (...args: any) => any
+    ? Parameters<EvaliteReporter[K]>
+    : never;
+};
+type EvaliteRunnerTestCase = Omit<RunnerTestCase, "meta"> & {
+  meta: { evalite: { result: Evalite.Result; duration: number } };
+};
 class MyEvaliteReporter extends EvaliteReporter {
-  onTestSuiteResult(testSuite: ad): void {
-    super.onTestSuiteResult(testSuite);
-    console.log(testSuite);
+  override reportTestSummary(...args: MethodInputs["reportTestSummary"]): void {
+    console.log("reportTestSummary", args);
+    const flattenSuiteTasks = (task: RunnerTask): Exclude<RunnerTask, { type: "suite" }>[] => {
+      if (task.type !== "suite") return [task];
+      return task.tasks.flatMap(flattenSuiteTasks);
+    };
+    const flatTasks = args[0].flatMap((test) =>
+      test.tasks.flatMap(flattenSuiteTasks),
+    ) as EvaliteRunnerTestCase[];
+    console.dir(
+      flatTasks.map((task) => ({
+        name: task.name,
+        type: task.type,
+        path: "filepath" in task ? String(task.filepath).replace(process.cwd() + "/", "") : null,
+        evalName: task.meta.evalite.result.evalName,
+        duration: task.meta.evalite.duration,
+        input: task.meta.evalite.result.input,
+        expect: task.meta.evalite.result.expected,
+        output: task.meta.evalite.result.output,
+        scores: task.meta.evalite.result.scores,
+      })),
+      { depth: null },
+    );
+    super.reportTestSummary(...args);
+    // console.dir(summary, { depth: null });
   }
+  // private renderTable(table: TableRow[]): void {
+  //   super.renderTable(table);
+  //   console.dir({ table }, { depth: null });
+  // }
 }
 
 export default defineConfig({
