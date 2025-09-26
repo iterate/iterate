c6f87c830b7d79472e6663e41b6ca6faeec9f845
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2025-09-26 18:31:34 +0100

    smaller sdk publish?

--- apps/os/tsdown.config.ts
+++ apps/os/tsdown.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["./sdk/index.ts"],
-  outDir: "./dist",
+  outDir: "../../sdk/dist",
   format: "esm",
   dts: true,
+  clean: true,
+  sourcemap: false,
+  noExternal: (id) =>
+    // almost everything is internal, so it gets bundled with the sdk. the exceptions need to be added as dependencies in the sdk package.json
+    // this is different from tsdown's default behavior, which is to make all prod dependencies external
+    !id.startsWith("zod/") && id !== "zod" && id !== "type-fest" && id !== "dedent",
 });
