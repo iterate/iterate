5cf5f33bef42c92e2e35ca41441e065ec25a1c2b
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2025-10-03 15:42:44 +0100

    rm

--- apps/os/evals/basic-conversation.eval.ts
+++ apps/os/evals/basic-conversation.eval.ts
@@ -17,78 +17,7 @@ beforeAll(async () => {
   trpcClient = await getAuthedTrpcClient();
 });
 
-if (false)
-  evalite("testing", {
-    data: async () => [
-      {
-        input: {
-          slug: "fruits",
-          messages: [
-            { message: "name a green fruit", expected: "a green fruit" },
-            { message: "name another", expected: "a green fruit, not the same as the first" },
-            { message: "name another", expected: "a green fruit, not the same as the 1st or 2nd" },
-          ],
-        },
-      },
-    ],
-    task: async (input) => {
-      return multiTrialTask(3, async () => {
-        const convo: string[] = [];
-        const scores: { score: number; reason: string }[] = [];
-        for (const message of input.messages) {
-          convo.push(`user: ${message.message}`);
-          const fruit = ["kiwi", "lime", "green grape", "apple"];
-          const selection = fruit[Math.floor(Math.random() * fruit.length)];
-          convo.push(`assistant: ${selection}`);
-          scores.push({ score: 1 - Math.random() * 0.1, reason: `${selection} is green i guess` });
-        }
-        return { convo, scores };
-      });
-    },
-    scorers: [
-      {
-        name: "one",
-        scorer: ({ output }) => ({
-          score: 1,
-          metadata: {
-            output: JSON.stringify(output),
-            reason: "uno",
-          },
-        }),
-      },
-    ],
-    columns: (x) => {
-      const turns = R.uniqueBy(
-        x.output.trials.flatMap((t) =>
-          t.result.scores.map((_s, i) => ({ index: i, name: `turn ${i + 1}` })),
-        ),
-        (t) => t.name,
-      );
-      return turns.map((turn) => {
-        const resultsAcrossTrials = x.output.trials.map((t) => t.result.scores[turn.index]);
-        return {
-          label: turn.name,
-          value: dedent`
-            \`\`\`
-            - mean: ${R.meanBy(resultsAcrossTrials, (s) => s.score)}
-            - min: ${minBy(resultsAcrossTrials, (s) => s.score)}
-            - median: ${medianBy(resultsAcrossTrials, (s) => s.score)}
-            - resultsAcrossTrials: ${JSON.stringify(resultsAcrossTrials)}
-            \`\`\`
-          `,
-        };
-      });
-    },
-  });
-
-const multiTrialTask = async <T>(count: number, runTrial: (index: number) => Promise<T>) => {
-  const results = await Promise.all(Array.from({ length: count }).map((_, i) => runTrial(i)));
-  const trials = results.map((r, i) => ({ trialIndex: i, trialName: `trial_${i + 1}`, result: r }));
-  return { trials };
-};
-
 const minBy = <T>(arr: T[], fn: (t: T) => number) => arr.map(fn).sort().at(0);
-const medianBy = <T>(arr: T[], fn: (t: T) => number) => R.median(arr.map(fn));
 
 evaliterate("agent knows when to end their turn", {
   trialCount: 2 || Number(process.env.EVAL_TRIAL_COUNT) || undefined,
