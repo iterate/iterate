bdbea02ac41a1dd44f8e265131dc60c41b08d712
Zakhar Davydov <zakharchk@gmail.com>
2025-10-18 16:07:47 -0700

    one migration, instead of two

--- apps/os/backend/db/migrations/0020_organic_metal_master.sql
+++ apps/os/backend/db/migrations/0020_organic_metal_master.sql
@@ -9,7 +9,12 @@ CREATE TABLE "slack_channel_estate_override" (
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
+DROP INDEX "provider_user_mapping_provider_id_external_id_index";--> statement-breakpoint
+ALTER TABLE "provider_user_mapping" ADD COLUMN "estate_id" text;--> statement-breakpoint
+ALTER TABLE "provider_user_mapping" ADD COLUMN "external_user_team_id" text;--> statement-breakpoint
 ALTER TABLE "slack_channel_estate_override" ADD CONSTRAINT "slack_channel_estate_override_estate_id_estate_id_fk" FOREIGN KEY ("estate_id") REFERENCES "public"."estate"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 CREATE UNIQUE INDEX "slack_channel_estate_override_slack_channel_id_slack_team_id_index" ON "slack_channel_estate_override" USING btree ("slack_channel_id","slack_team_id");--> statement-breakpoint
 CREATE INDEX "slack_channel_estate_override_slack_channel_id_index" ON "slack_channel_estate_override" USING btree ("slack_channel_id");--> statement-breakpoint
-CREATE INDEX "slack_channel_estate_override_estate_id_index" ON "slack_channel_estate_override" USING btree ("estate_id");
+CREATE INDEX "slack_channel_estate_override_estate_id_index" ON "slack_channel_estate_override" USING btree ("estate_id");--> statement-breakpoint
+ALTER TABLE "provider_user_mapping" ADD CONSTRAINT "provider_user_mapping_estate_id_estate_id_fk" FOREIGN KEY ("estate_id") REFERENCES "public"."estate"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+CREATE UNIQUE INDEX "provider_user_mapping_provider_id_estate_id_external_id_index" ON "provider_user_mapping" USING btree ("provider_id","estate_id","external_id");
