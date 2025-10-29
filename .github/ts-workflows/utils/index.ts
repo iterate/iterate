ab985f9c9cee6f8ab456138511f31c2378a764f5
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2025-10-29 10:40:11 +0000

    single quote

--- .github/ts-workflows/utils/index.ts
+++ .github/ts-workflows/utils/index.ts
@@ -1,4 +1,3 @@
 export const runsOn = {
-  "runs-on":
-    '${{ github.repository_owner == "iterate" && "depot-ubuntu-24.04-arm-4" || "ubuntu-24.04" }}',
+  "runs-on": `\${{ github.repository_owner == 'iterate' && 'depot-ubuntu-24.04-arm-4' || 'ubuntu-24.04' }}`,
 };
