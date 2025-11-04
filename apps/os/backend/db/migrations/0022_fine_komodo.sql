2ed179fc8911b4588c91a5ff065bd7d95d88e118
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2025-11-04 18:22:24 +0000

    set

--- apps/os/backend/db/migrations/0022_fine_komodo.sql
+++ apps/os/backend/db/migrations/0022_fine_komodo.sql
@@ -1,4 +1,9 @@
-ALTER TABLE "agent_instance_route" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
-DROP TABLE "agent_instance_route" CASCADE;--> statement-breakpoint
 ALTER TABLE "agent_instance" ADD COLUMN "routing_key" text;--> statement-breakpoint
 ALTER TABLE "agent_instance" ADD CONSTRAINT "agent_instance_routingKey_unique" UNIQUE("routing_key");
+
+update agent_instance
+set routing_key = (select routing_key from agent_instance_route where agent_instance_route.agent_instance_id = agent_instance.id limit 1)
+where routing_key is null;
+
+ALTER TABLE "agent_instance_route" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
+DROP TABLE "agent_instance_route" CASCADE;--> statement-breakpoint
