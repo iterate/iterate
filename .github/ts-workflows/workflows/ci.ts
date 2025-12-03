01b6a6479fc7a4571457b4456e0319c3cb71c91b
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2025-12-03 17:38:22 +0000

    default to stg for deploy outside of main

--- .github/ts-workflows/workflows/ci.ts
+++ .github/ts-workflows/workflows/ci.ts
@@ -46,7 +46,7 @@ export default {
           name: "Get environment variables",
           // todo: parse the PR number/body/whatever to get a stage like `pr_1234` and any other deployment flags
           run: dedent`
-            echo stage=\${{ inputs.stage || 'stg' }} >> $GITHUB_OUTPUT
+            echo stage=\${{ inputs.stage || ((github.ref_name == 'main' && 'prd') || 'stg') }} >> $GITHUB_OUTPUT
             echo release_name="v$(TS=Europe/London date +%Y-%m-%d-%H-%M-%S)" >> $GITHUB_OUTPUT
           `,
         },
