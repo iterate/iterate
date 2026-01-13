214fc4e0b24e006493bdb6961d2a00c157ac1ee4
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2026-01-13 09:11:44 +0000

    rm

--- e2e/org-creation.e2e.ts
+++ e2e/org-creation.e2e.ts
@@ -1,9 +1,9 @@
 import { login, createOrganization, test } from "./test-helpers.ts";
 
 test.describe("organization creation flow", () => {
-  test("should log in and create an organization", async ({ page, baseURL }) => {
+  test("should log in and create an organization", async ({ page }) => {
     const testEmail = `test-e2e-${Date.now()}+test@nustom.com`;
-    await login(page, testEmail, baseURL);
+    await login(page, testEmail);
     await createOrganization(page);
   });
 });
