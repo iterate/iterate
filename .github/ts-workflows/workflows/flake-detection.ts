ee80363630b8d59fda94a1a4cba633bc7e7143ef
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2026-01-13 15:27:03 +0000

    oops

--- .github/ts-workflows/workflows/flake-detection.ts
+++ .github/ts-workflows/workflows/flake-detection.ts
@@ -4,7 +4,7 @@ import * as utils from "../utils/index.ts";
 export default workflow({
   name: "Flake Detection",
   on: {
-    branch: ["**/*ciflak*"],
+    branch: ["**/*flake*"],
     schedule: [
       // Run daily at 6am UTC (7am UK winter / 8am UK summer)
       { cron: "0 6 * * *" },
