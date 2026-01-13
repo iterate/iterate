d9e3729adbfe264a0a4bbfd1c349c1114eddc6d8
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2026-01-13 22:05:09 +0000

    hopefully this works

--- apps/os/backend/db/migrations/0007_project_name_unique.sql
+++ apps/os/backend/db/migrations/0007_project_name_unique.sql
@@ -1,1 +1,14 @@
+-- a cte which checks for duplicated names and adds a random suffix to the name to allow adding a unique index after
+
+WITH duplicated_names AS (
+  SELECT name, organization_id, COUNT(*)
+  FROM project
+  GROUP BY name, organization_id
+  HAVING COUNT(*) > 1
+)
+SELECT name, organization_id, ROW_NUMBER() OVER (PARTITION BY name, organization_id ORDER BY id) as row_number
+FROM project
+WHERE name IN (SELECT name FROM duplicated_names);
+
+
 CREATE UNIQUE INDEX "project_organization_id_name_index" ON "project" USING btree ("organization_id","name");
