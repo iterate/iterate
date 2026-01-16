7441bb3764e3356bc8dcbe164adb01e9e1cf2470
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2026-01-16 22:25:37 +0000

    sharding

--- .github/ts-workflows/workflows/flake-detection.ts
+++ .github/ts-workflows/workflows/flake-detection.ts
@@ -2,6 +2,8 @@ import dedent from "dedent";
 import { workflow, uses } from "@jlarky/gha-ts/workflow-types";
 import * as utils from "../utils/index.ts";
 
+const shardTotal = 10;
+
 export default workflow({
   name: "Flake Detection",
   on: {
@@ -18,6 +20,12 @@ export default workflow({
     "flake-detection": {
       "timeout-minutes": 60,
       ...utils.runsOn,
+      strategy: {
+        "fail-fast": false,
+        matrix: {
+          shardIndex: Array.from({ length: shardTotal }, (_, i) => i + 1),
+        },
+      },
       steps: [
         ...utils.setupRepo,
         { run: "pnpm docker:up" },
@@ -31,7 +39,7 @@ export default workflow({
           "continue-on-error": true,
           run: dedent`
             mkdir -p test-results
-            pnpm spec --repeat-each=10 --reporter=json
+            pnpm spec --repeat-each=10 --reporter=json --shard=$\{{ matrix.shardIndex }}/${shardTotal}
           `,
           env: {
             PLAYWRIGHT_JSON_OUTPUT_FILE: "test-results/spec-results.json",
@@ -46,7 +54,7 @@ export default workflow({
           name: "Upload Test Results",
           if: "always()",
           ...uses("actions/upload-artifact@v4", {
-            name: "flaky-test-results",
+            name: "flaky-test-results-${{ matrix.shardIndex }}",
             path: "test-results",
             "retention-days": 30,
           }),
