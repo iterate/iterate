d3ea3ca8a96004e70c9511502ca447b28cc56dfd
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2026-01-16 20:56:02 +0000

    add trycatch impl

--- spec/spinner-waiter.ts
+++ spec/spinner-waiter.ts
@@ -107,11 +107,13 @@ function setup(page: Page) {
 
     locatorPrototype[`${method}_original`] = locatorPrototype[method];
     let value: (this: LocatorWithOriginal, ...args: unknown[]) => Promise<unknown>;
-    const impl = (process.env.SPINNER_WAITER_IMPL || "loop") as "loop" | "race";
+    const impl = (process.env.SPINNER_WAITER_IMPL || "loop") as "loop" | "race" | "trycatch";
     if (impl === "loop") {
       value = getLoopUntilVisibleImpl(method);
     } else if (impl === "race") {
       value = getRaceImpl(method);
+    } else if (impl === "trycatch") {
+      value = getTrycatchImpl(method);
     } else {
       impl satisfies never;
       throw new Error(`Unknown spinner waiter implementation: ${impl}`);
@@ -304,6 +306,93 @@ const getRaceImpl = (method: OverrideableMethod) => {
     }
 
     throw new Error(`Unknown race outcome: ${JSON.stringify(race)}`);
+  };
+};
+
+const getTrycatchImpl = (method: OverrideableMethod) => {
+  return async function trycatchImpl(this: LocatorWithOriginal, ...args: unknown[]) {
+    const optionIndex = oneArgMethods.includes(method as OneArgMethod) ? 1 : 0;
+    const options = (args.at(optionIndex) || {}) as Options<OverrideableMethod>;
+    const settings = getSettings();
+    const skipSpinnerCheck = settings.disabled || options?.skipSpinnerCheck;
+
+    settings.log(`${this}.${method}(...) ${JSON.stringify({ skipSpinnerCheck })}`);
+
+    const [attempt1] = await Promise.allSettled([
+      (this[`${method}_original`] as Function)(...args),
+    ]);
+
+    if (attempt1.status === "fulfilled") {
+      return attempt1.value;
+    }
+
+    if (skipSpinnerCheck) {
+      // bad luck, attempt1 is all you get
+      adjustError(attempt1.reason as Error);
+      throw attempt1.reason;
+    }
+
+    let called = false;
+
+    const callOriginal = async (argsList: unknown[]) => {
+      if (called) {
+        throw new Error("Original called more than once, this is a bug in spinner-waiter");
+      }
+
+      called = true;
+      return (this[`${method}_original`] as Function)(...argsList);
+    };
+
+    const spinnerSelector = settings.spinnerSelectors.join(",");
+    const spinnerLocator = this.page().locator(spinnerSelector) as LocatorWithOriginal;
+
+    const spinnerVisible = await spinnerLocator.isVisible();
+
+    if (!spinnerVisible) {
+      adjustError(attempt1.reason as Error, [
+        `If this is a slow operation, update the product code to add a spinner while it's running.`,
+        `This will improve the user experience and buy you more time for this assertion.`,
+        `To add a spinner, show any UI element matching this locator:`,
+        `  ${spinnerLocator}`,
+      ]);
+      throw attempt1.reason;
+    }
+
+    settings.log(
+      `${this} not visible, but the spinner is. racing between ${this}.${method}(...) and ${spinnerLocator} being hidden`,
+    );
+
+    const targetVisibleOrSpinnerRemoval = this.or(
+      this.page().locator(`body:not(:has(${spinnerSelector}))`),
+    ).first() as LocatorWithOriginal;
+
+    const [attempt2] = await Promise.allSettled([
+      targetVisibleOrSpinnerRemoval.waitFor_original({ timeout: settings.spinnerTimeout }),
+    ]);
+
+    if (attempt2.status === "rejected") {
+      adjustError(attempt2.reason as Error, [
+        `${this}.${method}(...) didn't succeed and spinner was still visible after ${settings.spinnerTimeout}ms, the UI is likely stuck.`,
+      ]);
+      throw attempt2.reason;
+    }
+
+    const targetVisible = await this.isVisible();
+
+    if (!targetVisible) {
+      // last chance, we expect to fail at this point
+      return await callOriginal(args).catch((e) => {
+        adjustError(e as Error, [
+          `The loading spinner is no longer visible but ${this}.${method} didn't succeed, make sure the spinner stays visible until the operation is complete.`,
+        ]);
+        throw e;
+      });
+    }
+
+    return await callOriginal(args).catch((e) => {
+      adjustError(e as Error);
+      throw e;
+    });
   };
 };
 
