203e42f5855284034ff51aaf4568f59892f62762
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2026-01-16 10:25:50 +0000

    draft: simpler try/catch based spinner-waiter implementation

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: 1,
+  workers: Number(process.env.PLAYWRIGHT_WORKERS) || 1,
   outputDir: "test-results/output",
   reporter: [
     ["list"],
