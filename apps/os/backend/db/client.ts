a91bcbb516fe3b1d8ff873a554829b3cba892744
NickBlow <nick.blow@live.com>
2026-02-10 23:21:05 +0100

    perf: Use neon driver

--- apps/os/backend/db/client.ts
+++ apps/os/backend/db/client.ts
@@ -1,21 +1,17 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
 import { env } from "../../env.ts";
 import * as schema from "./schema.ts";
 
 export const getDb = () => {
-  const client = postgres(env.DATABASE_URL, {
-    prepare: false,
-  });
-  return drizzle(client, { schema, casing: "snake_case" });
+  const pool = new Pool({ connectionString: env.DATABASE_URL });
+  return drizzle({ client: pool, schema, casing: "snake_case" });
 };
 
 /** Accepts any env-like object with DATABASE_URL */
 export const getDbWithEnv = (envParam: { DATABASE_URL: string }) => {
-  const client = postgres(envParam.DATABASE_URL, {
-    prepare: false,
-  });
-  return drizzle(client, { schema, casing: "snake_case" });
+  const pool = new Pool({ connectionString: envParam.DATABASE_URL });
+  return drizzle({ client: pool, schema, casing: "snake_case" });
 };
 
 export type DB = ReturnType<typeof getDb>;
