d49e81968904ceeaa8ac66691529c9b1fdb721df
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2026-02-10 00:19:33 +0000

    fix outbox migration which didnt seem to work on prod

---
+++ apps/os/backend/db/migrations/0018_fancy_stone_men.sql
@@ -0,0 +1,12 @@
+select pgmq.create('consumer_job_queue');
+
+CREATE TABLE "outbox_event" (
+	"id" bigserial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"payload" jsonb NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+DROP INDEX "project_organization_id_slug_index";--> statement-breakpoint
+ALTER TABLE "project" ADD CONSTRAINT "project_slug_unique" UNIQUE("slug");
