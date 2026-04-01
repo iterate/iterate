4b444df0332090c585d9f7825062b1d5ca418eb2
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2026-04-01 16:53:52 +0800

    cli

---
+++ ai-engineer-workshop/lib/codegen.cjs
@@ -0,0 +1,19 @@
+/** @type {import('eslint-plugin-codegen').Preset} */
+module.exports = ({ dependencies: { fs, path } }) => {
+  const baseDir = path.join(__dirname, "../jonas");
+  const files = fs.globSync("0*/**/*", { cwd: baseDir, exclude: ["node_modules", "web"] });
+  const entries = files
+    .filter((filename) => filename.endsWith(".ts") || filename.endsWith(".sh"))
+    .sort()
+    .map((filename) => {
+      const filepath = path.join(baseDir, filename);
+      const fileContent = fs.readFileSync(filepath, "utf8");
+      return [filename, fileContent];
+    });
+  return (
+    "// prettier-ignore\n" +
+    "const files = " +
+    JSON.stringify(Object.fromEntries(entries), null, 2) +
+    ";"
+  );
+};
