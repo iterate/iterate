d879baeecbb93ea0587025184e96482121f9963b
Jonas Templestein <242550+jonastemplestein@users.noreply.github.com>
2026-06-07 11:11:04 +0100

    Remove accidental React type changes

--- packages/ui/src/components/spinner.tsx
+++ packages/ui/src/components/spinner.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@iterate-com/ui/lib/utils";
 import { Loader2Icon } from "lucide-react";
 
-function Spinner({ className, ...props }: React.ComponentPropsWithoutRef<"svg">) {
+function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
   return (
     <Loader2Icon
       role="status"
