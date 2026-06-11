19003e050d1c08ae90fc244b8d42ac9c7c93d30f
Jonas Templestein <242550+jonastemplestein@users.noreply.github.com>
2026-06-11 07:19:32 +0100

    fix ui import meta env types

--- packages/ui/src/asset-modules.d.ts
+++ packages/ui/src/asset-modules.d.ts
@@ -2,3 +2,12 @@ declare module "*.svg" {
   const src: string;
   export default src;
 }
+
+interface ImportMetaEnv {
+  readonly SSR: boolean;
+  readonly DEV: boolean;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
