a4a02bad5fe0100ac7886e49133e439ae8ffbe3a
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2026-06-30 00:12:01 +0100

    fix

--- apps/minimal-itx-v4/itx.e2e.test.ts
+++ apps/minimal-itx-v4/itx.e2e.test.ts
@@ -177,8 +177,6 @@ describe("minimal itx v4", () => {
 
   test("Authenticated itx whoami returns principal", async () => {
     using session = withItxSession();
-    session.authenticate();
-    session.authenticate();
     using itx = session.authenticate({
       type: "token",
       token: {
